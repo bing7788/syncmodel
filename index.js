d2af2217158670fdc0611779b1d35e036ee371b8
bing7788 <1542721301@qq.com>
2019-01-05 15:49:51 +0800

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('socket.io');
